0b898d5571fa5d72ba4d36ee1475e61898e527a3
bungdikka <sunlightwk@163.com>
2020-04-01 09:50:26 +0800

    original push

---
+++ tiger-fastened-shut/src/test/java/com/moonlight/sso/TigerFastenedShutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonlight.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerFastenedShutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
